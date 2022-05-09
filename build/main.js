8f675a8c3a86171b08a82d309641edc5fdb8f419
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-05-09 04:07:47 +0200

    Udacity Full Stack Nanodegree

--- build/main.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import routes from './routes/index.js';
-var app = express();
-var port = 3000;
-app.listen(port, function () {
-    console.log("sever is up and working on port " + port);
-});
-app.use('/api', routes);
-export default app;
--- build/routes/index.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import footage from './api/footage.js';
-var routes = express.Router();
-routes.get('/', function (req, res) {
-    res.send("hello Ahmed, I'm working just fine!");
-});
-routes.use('/footage', footage);
-export default routes;
